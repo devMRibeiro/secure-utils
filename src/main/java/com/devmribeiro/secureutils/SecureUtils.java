04d560aa7434666601aee00f9fdab57366710ac6
devMRibeiro <michael.douglas2020@hotmail.com>
2025-03-24 04:15:14 -0300

    Fix documentation

--- src/main/java/com/devmribeiro/secureutils/SecureUtils.java
+++ src/main/java/com/devmribeiro/secureutils/SecureUtils.java
@@ -4,11 +4,11 @@ import java.security.SecureRandom;
 
 public abstract class SecureUtils {
 
-	protected static final String DEFAULT_CHARSET = "UTF-8";
 	protected static final SecureRandom RANDOM = new SecureRandom();
 
 	/**
-	 * @return Random byte array of size.
+	 * @param size The size to salt.
+	 * @return Random byte array of size {@code size}.
 	 */
 	public static byte[] generateSalt(int size) {
 		byte[] salt = new byte[size];
