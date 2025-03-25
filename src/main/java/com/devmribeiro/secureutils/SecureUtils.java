8ec5011e5c66879c370f3c4aea1409bf3324dfe1
devMRibeiro <michael.douglas2020@hotmail.com>
2025-03-25 01:11:01 -0300

    Creates base64 enconder and decoder

--- src/main/java/com/devmribeiro/secureutils/SecureUtils.java
+++ src/main/java/com/devmribeiro/secureutils/SecureUtils.java
@@ -1,18 +1,41 @@
 package com.devmribeiro.secureutils;
 
 import java.security.SecureRandom;
+import java.util.Base64;
 
 public abstract class SecureUtils {
 
 	protected static final SecureRandom RANDOM = new SecureRandom();
 
 	/**
-	 * @param size The size to salt.
-	 * @return Random byte array of size {@code size}.
+	 * Generates a random salt of the specified size.
+	 * 
+	 * @param size The size (in bytes) of the salt to generate.
+	 * @return A random byte array of the specified size, which can be used as a salt.
 	 */
 	public static byte[] generateSalt(int size) {
 		byte[] salt = new byte[size];
 		RANDOM.nextBytes(salt);
 		return salt;
+	}
+	
+	/**
+	 * Encodes a byte array into a Base64-encoded string.
+	 * 
+	 * @param input The byte array to be encoded in Base64.
+	 * @return A string containing the Base64-encoded representation of the input byte array.
+	 */
+	public static String base64Enconder(byte[] input) {
+		return Base64.getEncoder().encodeToString(input);
+	}
+	
+	/**
+	 * Decodes a Base64-encoded byte array.
+	 * 
+	 * @param input The byte array encoded in Base64.
+	 * @return A byte array containing the decoded data.
+	 */
+	public static byte[] base64Decoder(byte[] input) {
+		return Base64.getDecoder().decode(input);
 	}
 }
