7958227eacc6360ed679253ae1b70e48ff6c9a0b
devMRibeiro <michael.douglas2020@hotmail.com>
2025-04-05 19:39:42 -0300

    create key encryption method

--- src/main/java/com/devmribeiro/secureutils/jwt/JWT.java
+++ src/main/java/com/devmribeiro/secureutils/jwt/JWT.java
@@ -2,8 +2,10 @@ package com.devmribeiro.secureutils.jwt;
 
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Date;
 import java.util.List;
@@ -34,5 +36,12 @@ public class JWT {
         byte[] keyBytes = SecureUtils.base64Decoder(jwtPublicKey.getBytes());
         X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(keyBytes);
         return keyFactory.generatePublic(x509EncodedKeySpec);
+    }
+
+    public PrivateKey generateJwtKeyEncryption(String jwtPrivateKey) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        byte[] keyBytes = SecureUtils.base64Decoder(jwtPrivateKey.getBytes());
+        PKCS8EncodedKeySpec pkcs8EncodedKeySpec=new PKCS8EncodedKeySpec(keyBytes);
+        return keyFactory.generatePrivate(pkcs8EncodedKeySpec);
     }
 }
