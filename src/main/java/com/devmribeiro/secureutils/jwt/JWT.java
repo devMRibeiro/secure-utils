33a10109222caac758475f19090dbedfadacd318
devMRibeiro <michael.douglas2020@hotmail.com>
2025-04-05 19:38:05 -0300

    create JWT class with methods to generate access token and key decryption

---
+++ src/main/java/com/devmribeiro/secureutils/jwt/JWT.java
@@ -0,0 +1,38 @@
+package com.devmribeiro.secureutils.jwt;
+
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Date;
+import java.util.List;
+
+import com.devmribeiro.secureutils.SecureUtils;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+/**
+ * @author Michael D. Ribeiro
+ * @since 1.4
+ */
+public class JWT {
+	int accessExpirationMs = 9600000;
+    public String generateAccessToken(String userName, List<String> roleArray, String jwtPrivateKey) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        return Jwts.builder()
+                .setSubject(userName)
+                .claim("roles", roleArray)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + accessExpirationMs))
+                .signWith(generateJwtKeyDecryption(jwtPrivateKey), SignatureAlgorithm.RS256)
+                .compact();
+    }
+
+    public PublicKey generateJwtKeyDecryption(String jwtPublicKey) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        byte[] keyBytes = SecureUtils.base64Decoder(jwtPublicKey.getBytes());
+        X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(keyBytes);
+        return keyFactory.generatePublic(x509EncodedKeySpec);
+    }
+}
