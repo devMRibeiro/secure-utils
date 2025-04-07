ecb3558697002eba54fa18203b0e40565310e6b2
devMRibeiro <michael.douglas2020@hotmail.com>
2025-04-06 23:31:46 -0300

    Refactor: move RSA_ALGORITHM attribute from RsaUtils to SecureUtils class to allow access by JwtUtils class

--- src/main/java/com/devmribeiro/secureutils/SecureUtils.java
+++ src/main/java/com/devmribeiro/secureutils/SecureUtils.java
@@ -4,28 +4,25 @@ import java.security.SecureRandom;
 import java.util.Base64;
 
 /**
- * Utility class providing cryptographic-related helper methods.  
- * <p>  
- * This class includes methods for generating secure random salts and encoding/decoding data  
- * using the Base64 format. It serves as a utility for handling basic cryptographic operations  
- * in a secure manner.  
- * </p>  
- * <p>  
- * This class is abstract and cannot be instantiated. All methods are static and should be  
- * accessed directly.  
- * </p>  
- *  
- * @author Michael D. Ribeiro  
- * @version 1.1 (March 2025)  
- * @since 1.0  
+ * Utility class providing cryptographic-related helper methods.
+ * <p>
+ * This class includes methods for generating secure random salts and encoding/decoding data
+ * using the Base64 format. It serves as a utility for handling basic cryptographic operations
+ * in a secure manner.
+ * </p>
+ *
+ * @author Michael D. Ribeiro
+ * @since 1.0
  */
-public abstract class SecureUtils {
+public class SecureUtils {
 
 	public static final SecureRandom RANDOM = new SecureRandom();
+	public static final String AES_ALGORITHM = "AES";
+	public static final String RSA_ALGORITHM = "RSA";
 
 	/**
 	 * Generates a random salt of the specified size.
-	 * 
+	 *
 	 * @param size The size (in bytes) of the salt to generate.
 	 * @return A random byte array of the specified size, which can be used as a salt.
 	 */
@@ -34,10 +31,10 @@ public abstract class SecureUtils {
 		RANDOM.nextBytes(salt);
 		return salt;
 	}
-	
+
 	/**
 	 * Encodes a byte array into a Base64-encoded string.
-	 * 
+	 *
 	 * @param input The byte array to be encoded in Base64.
 	 * @return A string containing the Base64-encoded representation of the input byte array.
 	 * @since 1.1
@@ -45,7 +42,7 @@ public abstract class SecureUtils {
 	public static String base64Enconder(byte[] input) {
 		return Base64.getEncoder().encodeToString(input);
 	}
-	
+
 	/**
 	 * Decodes a Base64-encoded byte array.
 	 * 
@@ -53,7 +50,7 @@ public abstract class SecureUtils {
 	 * @return A byte array containing the decoded data.
 	 * @since 1.1
 	 */
-	public static byte[] base64Decoder(byte[] input) {
-		return Base64.getDecoder().decode(input);
+	public static byte[] base64Decoder(byte[] base64Encoded) {
+		return Base64.getDecoder().decode(base64Encoded);
 	}
 }
